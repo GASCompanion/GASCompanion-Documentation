7ded9b615b6170d05e9a04503280a840247c305b
Mickael Daniel <daniel.mickael@gmail.com>
2022-04-18 14:10:21 +0200

    Cleanup old repo

--- src/utils/slugify.js
+++
@@ -1,9 +0,0 @@
-const { kebabCase } = require(`lodash`)
-
-const slugify = (source) => {
-    const slug = kebabCase(source)
-
-   return `/${slug}`.replace(/\/\/+/g, `/`)
-}
-
-module.exports = slugify
